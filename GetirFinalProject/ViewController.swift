39f2f7fd41a93808fc30a6d649be7be7e8906f9b
borakocapinar <borakocapinar2001@gmail.com>
2024-04-10 01:33:01 +0300

    set project for programmatic ui

---
+++ GetirFinalProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GetirFinalProject
+//
+//  Created by BORA KOCAPINAR on 10.04.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.systemPurple
+    }
+
+
+}
+
